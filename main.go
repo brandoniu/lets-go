63321913f82e9df1c1a0f011dcde2010c3af588e
niubrandon <niubrandon@gmail.com>
2023-10-14 15:35:41 -0400

    refactor into repository pattern

--- main.go
+++ main.go
@@ -1,36 +1,27 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 	"os"
 
 	"github.com/brandoniu/lets-go/api"
-	"github.com/brandoniu/lets-go/store"
-
-	_ "github.com/lib/pq"
+	"github.com/brandoniu/lets-go/repository"
+	"github.com/brandoniu/lets-go/service"
 )
 
 func main() {
-	databaseURL := os.Getenv("DATABASE_URL")
-	if databaseURL == "" {
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
 		log.Fatal("DATABASE_URL not set")
 	}
-	db, err := sql.Open("postgres", databaseURL)
+	
+	bookRepo, err := repository.NewPostgresBookRepository(connString)
 	if err != nil {
-		log.Fatal("Failed to open database:", err)
+		log.Fatal("Failed to connect to the database")
 	}
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Failed to communicate with database:", err)
-	}
-
-	store := store.New(db)
-
-	app := api.NewAPI(store)
-
-	app.Engine.Run(":8080")
-
+	BookService := service.NewBookService(bookRepo)
+	apiHandler := api.NewAPI(BookService)
+	
+	apiHandler.Run(":8080")
 	
 }
